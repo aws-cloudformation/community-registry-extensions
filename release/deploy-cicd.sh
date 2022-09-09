30981797ace8f3155d6e087876dd1e55dea00ce6
Eric Z. Beard <ezbeard@amazon.com>
2022-09-09 12:11:59 -0700

    cicd template deploys

---
+++ release/deploy-cicd.sh
@@ -0,0 +1,4 @@
+rain --profile cep-cicd deploy -y\
+    --params RepoId=ericzbeard/community-registry-extensions,Branch=main,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:531337079465:connection/dba82310-0865-4c7e-a874-adf7a2fdbb7a \
+    release/cicd.yml
+
