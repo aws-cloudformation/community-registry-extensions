365befa74ee1cadde38af79cdc669f4bba95ffd4
Eric Z. Beard <ezbeard@amazon.com>
2022-09-09 17:08:54 -0700

    Removed step functions

--- release/deploy-cicd.sh
+++ release/deploy-cicd.sh
@@ -1,4 +1,10 @@
+#!/bin/sh
+
+set -eou pipefail
+
+cfn-lint release/cicd.yml
+
 rain --profile cep-cicd deploy -y\
-    --params RepoId=ericzbeard/community-registry-extensions,Branch=main,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:531337079465:connection/dba82310-0865-4c7e-a874-adf7a2fdbb7a \
+    --params RepoId=ericzbeard/community-registry-extensions,Branch=release-process,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:531337079465:connection/dba82310-0865-4c7e-a874-adf7a2fdbb7a \
     release/cicd.yml
 
