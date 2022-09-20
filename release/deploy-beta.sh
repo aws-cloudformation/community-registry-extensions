7b9e2ec7e1f1dae4412a4ff3c64d73b9f23fcf8a
Eric Z. Beard <ezbeard@amazon.com>
2022-09-20 10:01:32 -0700

    Beta CICD and integ stacks

--- release/deploy-beta.sh
+++ release/deploy-beta.sh
@@ -5,6 +5,6 @@ set -eou pipefail
 cfn-lint release/cicd.yml
 
 rain --profile cep-beta deploy -y\
-    --params RepoId=ericzbeard/community-registry-extensions,Branch=beta,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:676545906896:connection/8310f1ae-92ef-415a-ae1a-176a04ce8ed4,Env=beta \
+    --params RepoId=ericzbeard/community-registry-extensions,Branch=beta,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:676545906896:connection/8310f1ae-92ef-415a-ae1a-176a04ce8ed4,Env=beta,Prefix=AwsCommunity,PrefixLower=awscommunity \
     release/cicd.yml cep-beta
 
