3d9cb0244d7deb96ef9025839b186d2a21556b48
Eric Z. Beard <ericzbeard@gmail.com>
2022-09-20 10:43:47 -0700

    chore(release): Beta template and integ tests (#34)
    * beta account
    
    * Beta CICD and integ stacks
    
    Co-authored-by: Eric Z. Beard <ezbeard@amazon.com>

--- release/deploy-cicd.sh
+++ release/deploy-cicd.sh
@@ -5,6 +5,6 @@ set -eou pipefail
 cfn-lint release/cicd.yml
 
 rain --profile cep-cicd deploy -y\
-    --params RepoId=ericzbeard/community-registry-extensions,Branch=hook-cicd,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:531337079465:connection/d685215c-9aa8-40ed-9a85-af65f6e623a5 \
+    --params RepoId=ericzbeard/community-registry-extensions,Branch=hook-cicd,GitHubConnectionArn=arn:aws:codestar-connections:us-east-1:531337079465:connection/d685215c-9aa8-40ed-9a85-af65f6e623a5,Env=cicd,Prefix=AwsCommunity,PrefixLower=awscommunity \
     release/cicd.yml
 
