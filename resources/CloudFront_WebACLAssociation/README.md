0063265650895f2d213721673f6a5680c429b2ef
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2022-09-23 22:33:39 +0530

    feat(resource): CloudFront Web ACL Association (#35)
    * resource for cloudfront webACL association
    rebase
    
    * untracking files
    
    * setup file, inputs files and readme
    
    * fixed resource folder name
    
    * fixed linting
    
    * input file typo fix
    
    * more lint fixes
    
    * linting updates
    
    * lint updates
    
    * cicd resrouces added
    
    * gitignore and cicd.yml updates
    
    * restoring gitignore
    
    * restoring gitignore
    
    * gitignore upadates
    
    * removing mac file
    
    * renaming inputs folder
    
    Co-authored-by: Suraj Tripathi <acc.surajtripthi@gmail.com>
    Co-authored-by: Kevin DeJong <kddejong@amazon.com>

---
+++ resources/CloudFront_WebACLAssociation/README.md
@@ -0,0 +1,54 @@
+# AwsCommunity::CloudFront::WebACLAssociation
+
+ Currently, CloudFormation ‘AWS::WAFv2::WebACLAssociation’ resource has a limitation that a new WebACL provisioned using CloudFormation cannot be associated to an existing CloudFront distribution (created outside of the CloudFormation template). For associating a web ACL to resources in CloudFormation, we can create a resource ‘AWS::WAFv2::WebACLAssociation’. However, this resource type only supports below ResourceArn values for Application Load Balancer, API Gateway and AppSync GraphQL API. This registry extension resource type associates a WebACL to a CloudFront distribution ARN. 
+
+
+## Sample template
+
+```yml
+AWSTemplateFormatVersion: "2010-09-09"
+Description: "This is to protect the CloudFront"
+Parameters : 
+  DistributionARN : 
+    Type : String 
+  WebAclARN : 
+    Type : String 
+Resources:
+  CFwebACLAssociation:
+    Type : AwsCommunity::CloudFront::WebACLAssociation
+    Properties :
+      DistributionArn: !Ref DistributionARN
+      WebAclArn: !Ref WebAclARN
+```
+
+## Development
+
+Open two tabs in your terminal.
+
+Create a virtual environment.
+
+```sh
+cd resources/CloudFront_WebACLAssociation
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements-dev.txt
+```
+
+In the other tab, run SAM local:
+
+```sh
+cd resources/CloudFront_WebACLAssociation
+source .env/bin/activate
+sam local start-lambda
+```
+
+Keep in mind that SAM local testing only simulates lambda locally, it will make SDK 
+calls into your account and create resources!
+
+Create the setup stack so that contract tests have something to interact with in your account.
+
+```sh
+aws cloudformation create-stack --stack-name cloudfront-webacl-association-setup \
+    --template-body file://test/setup.yml
+```
+
---
+++ resources/CloudFront_WebACLAssociation/src/awscommunity_cloudfront_webaclassociation/handlers.py
@@ -0,0 +1,247 @@
+"""Resources Handler """
+import logging
+from typing import Optional
+
+
+from cloudformation_cli_python_lib import (
+    Action,
+    HandlerErrorCode,
+    OperationStatus,
+    ProgressEvent,
+    Resource,
+    SessionProxy,
+)
+
+from .models import ResourceHandlerRequest, ResourceModel
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+
+# Set logging level
+# (https://docs.python.org/3/library/logging.html#levels)
+# Consider using logging.DEBUG for development and testing only
+LOG.setLevel(logging.DEBUG)
+
+TYPE_NAME = "AwsCommunity::CloudFront::WebACLAssociation"
+
+resource = Resource(TYPE_NAME, ResourceModel)
+test_entrypoint = resource.test_entrypoint
+
+
+@resource.handler(Action.CREATE)
+def create_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+) -> ProgressEvent:
+    """Create Handler Method """
+    model = request.desiredResourceState
+    # Example:
+    try:
+
+        # primary identifier from example
+        client = _get_session_client(
+            session,
+            "cloudfront",
+        )
+
+        webacl_association_kwargs = _webacl_association_helper(
+            model=model
+        )
+
+        cloudfront_distribution_id = (
+            webacl_association_kwargs["DistributionArn"].split(":")[-1].split("/")[-1]
+        )
+        web_acl_id = (
+            webacl_association_kwargs["WebAclArn"].split(":")[-1].split("/")[-1]
+        )
+        response = client.get_distribution_config(Id=cloudfront_distribution_id)
+        old_config = response["DistributionConfig"]
+        if old_config["WebACLId"] == web_acl_id:
+            return ProgressEvent(
+                status=OperationStatus.FAILED,
+                HandlerErrorCode=HandlerErrorCode.AlreadyExists,
+                Message="The provided web ACL association configurations already exists",
+            )
+        etag = response["ResponseMetadata"]["HTTPHeaders"]["etag"]
+        new_config = old_config
+        new_config["WebACLId"] = webacl_association_kwargs["WebAclArn"]
+        client.update_distribution(
+            DistributionConfig=new_config,
+            Id=cloudfront_distribution_id,
+            IfMatch=etag,
+        )
+        model.Name = webacl_association_kwargs["DistributionArn"]
+
+    except Exception as e:
+        return ProgressEvent.failed(
+            HandlerErrorCode.NotFound,
+            "Resource creation failed due to some service exception. " + str(e),
+        )
+
+    return ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=model)
+
+
+@resource.handler(Action.UPDATE)
+def update_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+) -> ProgressEvent:
+    """Update Handler Method """
+    model = request.desiredResourceState
+    # TODO: put code here
+    try:
+
+        # primary identifier from example
+        client = _get_session_client(
+            session,
+            "cloudfront",
+        )
+        webacl_association_kwargs = _webacl_association_helper(
+            model=model
+        )
+
+        cloudfront_distribution_id = (
+            webacl_association_kwargs["DistributionArn"].split(":")[-1].split("/")[-1]
+        )
+
+        response = client.get_distribution_config(Id=cloudfront_distribution_id)
+        old_config = response["DistributionConfig"]
+        if old_config["WebACLId"] == "":
+            LOG.debug("UPDATE working cp 3")
+            return ProgressEvent.failed(
+                HandlerErrorCode.NotFound,
+                "The provided web ACL association configurations does not exists",
+            )
+        etag = response["ResponseMetadata"]["HTTPHeaders"]["etag"]
+        new_config = old_config
+        new_config["WebACLId"] = webacl_association_kwargs["WebAclArn"]
+        client.update_distribution(
+            DistributionConfig=new_config,
+            Id=cloudfront_distribution_id,
+            IfMatch=etag,
+        )
+        LOG.debug("UPDATE working cp 5")
+    except Exception as e:
+        return ProgressEvent.failed(
+            HandlerErrorCode.NotFound,
+            "Resource update failed due to some service exception. " + str(e),
+        )
+
+    return ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=model)
+
+
+@resource.handler(Action.DELETE)
+def delete_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+) -> ProgressEvent:
+    """Delete Handler Method """
+    model = request.desiredResourceState
+    # TODO: put code here
+    try:
+
+        # primary identifier from example
+        client = _get_session_client(
+            session,
+            "cloudfront",
+        )
+
+        webacl_association_kwargs = _webacl_association_helper(
+            model=model
+        )
+
+        cloudfront_distribution_id = (
+            webacl_association_kwargs["DistributionArn"].split(":")[-1].split("/")[-1]
+        )
+
+        response = client.get_distribution_config(Id=cloudfront_distribution_id)
+        old_config = response["DistributionConfig"]
+
+        if old_config["WebACLId"] == "":
+            return ProgressEvent.failed(
+                HandlerErrorCode.NotFound,
+                "The provided web ACL association configurations does not exists",
+            )
+        etag = response["ResponseMetadata"]["HTTPHeaders"]["etag"]
+        new_config = old_config
+        new_config["WebACLId"] = ""
+        client.update_distribution(
+            DistributionConfig=new_config, Id=cloudfront_distribution_id, IfMatch=etag
+        )
+
+    except Exception as e:
+        return ProgressEvent.failed(
+            HandlerErrorCode.NotFound,
+            "Resource delete failed due to some service exception. " + str(e),
+        )
+
+    return ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=None)
+
+
+@resource.handler(Action.READ)
+def read_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+) -> ProgressEvent:
+    """Read Handler Method """
+    model = request.desiredResourceState
+    # primary identifier from example
+    try:
+
+        client = _get_session_client(
+            session,
+            "cloudfront",
+        )
+
+        webacl_association_kwargs = _webacl_association_helper(
+            model=model
+        )
+
+        cloudfront_distribution_id = (
+            webacl_association_kwargs["DistributionArn"].split(":")[-1].split("/")[-1]
+        )
+        response = client.get_distribution_config(Id=cloudfront_distribution_id)
+        old_config = response["DistributionConfig"]
+        if old_config["WebACLId"] == "":
+            return ProgressEvent.failed(
+                HandlerErrorCode.NotFound,
+                "The provided web ACL association configurations does not exists",
+            )
+    except Exception as e:
+        return ProgressEvent.failed(
+            HandlerErrorCode.NotFound,
+            "The provided web ACL association configurations does not exists."
+           + " Or the read operation failed in the custom reesource type. " + e,
+        )
+    return ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=model)
+
+
+def _get_session_client(
+    session: Optional[SessionProxy],
+    service_name: str,
+) -> type:
+    """Create and return a session client for a given service"""
+
+    if isinstance(
+        session,
+        SessionProxy,
+    ):
+        client = session.client(
+            service_name,
+        )
+        return client
+    return None
+
+
+def _webacl_association_helper(
+    model: ResourceModel,
+) -> dict:
+    """Create and return a dictionary of arguments for import_key_pair()"""
+    LOG.debug("_webacl_association_helper()")
+
+    webacl_association_kwargs = {
+        "DistributionArn": model.DistributionArn,
+        "WebAclArn": model.WebAclArn,
+    }
+
+    return webacl_association_kwargs
