da5c93568b61d37f8892cf6adee8363267d24597
Kevin DeJong <kddejong@amazon.com>
2022-09-27 15:41:55 -0700

    Add resource DynamoDB Item to create a record in a DynamoDB Table

---
+++ resources/DynamoDB_Item/docs/item.md
@@ -0,0 +1,30 @@
+# AwsCommunity::DynamoDB::Item Item
+
+## Syntax
+
+To declare this entity in your AWS CloudFormation template, use the following syntax:
+
+### JSON
+
+<pre>
+{
+    "<a href="#^[a-za-z0-9_@./#&+-]{1,255}$" title="^[A-Za-z0-9_@./#&+-]{1,255}$">^[A-Za-z0-9_@./#&+-]{1,255}$</a>" : <i><a href="attributevalue.md">AttributeValue</a></i>
+}
+</pre>
+
+### YAML
+
+<pre>
+<a href="#^[a-za-z0-9_@./#&+-]{1,255}$" title="^[A-Za-z0-9_@./#&+-]{1,255}$">^[A-Za-z0-9_@./#&+-]{1,255}$</a>: <i><a href="attributevalue.md">AttributeValue</a></i>
+</pre>
+
+## Properties
+
+#### \^[A-Za-z0-9_@./#&+-]{1,255}$
+
+_Required_: No
+
+_Type_: <a href="attributevalue.md">AttributeValue</a>
+
+_Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
+
---
+++ resources/DynamoDB_Item/src/awscommunity_dynamodb_item/handlers.py
@@ -0,0 +1,210 @@
+import logging
+import json
+from botocore.exceptions import ClientError
+from typing import Any, MutableMapping, Optional
+
+from cloudformation_cli_python_lib import (
+    Action,
+    HandlerErrorCode,
+    OperationStatus,
+    ProgressEvent,
+    Resource,
+    SessionProxy,
+    exceptions,
+    identifier_utils,
+)
+
+from .models import ResourceHandlerRequest, ResourceModel
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+TYPE_NAME = "AwsCommunity::DynamoDB::Item"
+
+resource = Resource(TYPE_NAME, ResourceModel)
+test_entrypoint = resource.test_entrypoint
+
+
+@resource.handler(Action.CREATE)
+def create_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=model,
+    )
+   
+    try:
+        if model is not None:
+            item = model.Item
+            if item is None:
+               item = {}
+
+            condition = ""
+            if model.Key is not None:
+                for k, v in model.Key.items():
+                    item[k] = v
+                    if condition != "":
+                        condition = f"{condition} AND attribute_not_exists({k})"
+                    else:
+                        condition = f"attribute_not_exists({k})"
+            if isinstance(session, SessionProxy):
+                client = session.client("dynamodb")
+                client.put_item(
+                    TableName=model.TableName,
+                    Item=item,
+                    ConditionExpression=condition
+                )
+        # Setting Status to success will signal to cfn that the operation is complete
+        progress.status = OperationStatus.SUCCESS
+    except ClientError as e:
+        if e.response['Error']['Code']=='ConditionalCheckFailedException':
+            return ProgressEvent.failed(HandlerErrorCode.AlreadyExists, f"item already exists")
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+    except Exception as e:
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+
+    return read_handler(session, request, callback_context)
+
+
+@resource.handler(Action.UPDATE)
+def update_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=model,
+    )
+    try:
+        if model is not None:
+            item = model.Item
+            if item is None:
+               item = {}
+
+            condition = ""
+            if model.Key is not None:
+                for k, v in model.Key.items():
+                    item[k] = v
+                    if condition != "":
+                        condition = f"{condition} AND attribute_exists({k})"
+                    else:
+                        condition = f"attribute_exists({k})"
+            if isinstance(session, SessionProxy):
+                
+                client = session.client("dynamodb")
+                client.put_item(
+                    TableName=model.TableName,
+                    Item=item,
+                    ConditionExpression=condition
+                )
+        # Setting Status to success will signal to cfn that the operation is complete
+        progress.status = OperationStatus.SUCCESS
+    except ClientError as e:
+        if e.response['Error']['Code']=='ConditionalCheckFailedException':
+            return ProgressEvent.failed(HandlerErrorCode.NotFound, f"item does not exist")
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+    except Exception as e:
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+
+    return read_handler(session, request, callback_context)
+
+
+@resource.handler(Action.DELETE)
+def delete_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
+        resourceModel=None,
+    )
+    try:
+        if model is not None:
+            if isinstance(session, SessionProxy):
+                
+                condition = ""
+                if model.Key is not None:
+                    for k, v in model.Key.items():
+                        if condition != "":
+                            condition = f"{condition} AND attribute_exists({k})"
+                        else:
+                            condition = f"attribute_exists({k})"
+                client = session.client("dynamodb")
+                client.delete_item(
+                    TableName=model.TableName,
+                    Key=model.Key,
+                    ConditionExpression=condition,
+                )
+        # Setting Status to success will signal to cfn that the operation is complete
+        progress.status = OperationStatus.SUCCESS
+    except ClientError as e:
+        if e.response['Error']['Code']=='ConditionalCheckFailedException':
+            return ProgressEvent.failed(HandlerErrorCode.NotFound, f"item does not exist")
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+    except Exception as e:
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+
+    return progress
+
+
+@resource.handler(Action.READ)
+def read_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    outputModel: ResourceModel
+    
+    try:
+        if model is not None:
+            if isinstance(session, SessionProxy):               
+                client = session.client("dynamodb")
+                result = client.get_item(
+                    TableName=model.TableName,
+                    Key=model.Key,
+                    ConsistentRead=True,
+                )
+                if result.get('Item', None) is not None:
+                    keys = {}
+                    attributes = {}
+                    for key in model.Key.keys():
+                        keys[key] = result['Item'][key]
+                    for key in model.Item.keys():
+                        attributes[key] = result['Item'][key]
+                    outputModel = ResourceModel(TableName=model.TableName, Key=keys, Item=attributes)
+                    return ProgressEvent(
+                        status=OperationStatus.SUCCESS,
+                        resourceModel=outputModel,
+                    )
+                else:
+                    return ProgressEvent.failed(HandlerErrorCode.NotFound, f"resource not found")
+    except ClientError as e:
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+    except Exception as e:
+        return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"got error {e}")
+
+    return ProgressEvent(
+        status=OperationStatus.SUCCESS,
+        resourceModel=outputModel,
+    )
+
+
+@resource.handler(Action.LIST)
+def list_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    # TODO: put code here
+    return ProgressEvent(
+        status=OperationStatus.SUCCESS,
+        resourceModels=[],
+    )
