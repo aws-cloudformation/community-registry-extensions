7a5175717395aa4a3f49c092ad16d69916347076
Eric Z. Beard <ezbeard@amazon.com>
2022-10-06 12:58:41 -0700

    Fix deploy scripts

--- release/deploy-beta.sh
+++ release/deploy-beta.sh
@@ -2,11 +2,11 @@
 
 set -eou pipefail
 
-cfn-lint release/cicd.yml -i W3002
+cfn-lint cicd.yml -i W3002
 
-aws --profile cep-beta cloudformation package --template-file release/cicd.yml --s3-bucket community-registry-extensions-beta-packages > /tmp/cicd.yml
+aws --profile cep-beta cloudformation package --template-file cicd.yml --s3-bucket community-registry-extensions-beta-packages > cicd-package.yml
 
 rain --profile cep-beta deploy -y\
     --params Env=beta,Prefix=AwsCommunity,PrefixLower=awscommunity,GitBranch=release,GitUrl=https://github.com/aws-cloudformation/community-registry-extensions.git,GitHubSecretArn=arn:aws:secretsmanager:us-east-1:676545906896:secret:cep-github-webhook-secret-L1HEni \
-    /tmp/cicd.yml cep-beta
+    cicd-package.yml cep-beta
 
--- release/deploy-cicd.sh
+++ release/deploy-cicd.sh
@@ -2,11 +2,11 @@
 
 set -eou pipefail
 
-cfn-lint release/cicd.yml -i W3002
+cfn-lint cicd.yml -i W3002
 
-aws --profile cep-cicd cloudformation package --template-file release/cicd.yml --s3-bucket community-registry-extensions-cicd-packages > /tmp/cicd.yml
+aws --profile cep-cicd cloudformation package --template-file cicd.yml --s3-bucket community-registry-extensions-cicd-packages > cicd-package.yml
 
 rain --profile cep-cicd deploy -y\
     --params Env=cicd,Prefix=AwsCommunity,PrefixLower=awscommunity,GitBranch=main,GitUrl=https://github.com/aws-cloudformation/community-registry-extensions.git,GitHubSecretArn=arn:aws:secretsmanager:us-east-1:531337079465:secret:github-webhook-kqkHT9 \
-    /tmp/cicd.yml cep-cicd
+    cicd-package.yml cep-cicd
 
