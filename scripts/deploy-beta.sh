9fb057d55c20cdc25cbdc75f89077b8b8cb6c772
Eric Z. Beard <ezbeard@amazon.com>
2022-10-19 07:45:31 -0700

    Incorporate KMS key to support cross account pipeline s3 deploy

--- scripts/deploy-beta.sh
+++ scripts/deploy-beta.sh
@@ -11,5 +11,6 @@ export GIT_BRANCH=release
 export GIT_URL=https://github.com/aws-cloudformation/community-registry-extensions.git
 export GITHUB_SECRET_ARN=arn:aws:secretsmanager:us-east-1:676545906896:secret:cep-github-webhook-secret-L1HEni
 export PUBLISH_BUILD_BUCKET_NAME=community-registry-extensions-publish-build
+export PROD_ACCOUNT_ID=387586997764
 
 ./deploy-beta.sh
--- scripts/deploy-cicd.sh
+++ scripts/deploy-cicd.sh
@@ -11,5 +11,6 @@ export GIT_BRANCH=main
 export GIT_URL=https://github.com/aws-cloudformation/community-registry-extensions.git
 export GITHUB_SECRET_ARN=arn:aws:secretsmanager:us-east-1:531337079465:secret:github-webhook-kqkHT9
 export PUBLISH_BUILD_BUCKET_NAME=community-registry-extensions-publish-build
+export PROD_ACCOUNT_ID=387586997764
 
 ./deploy-cicd.sh
