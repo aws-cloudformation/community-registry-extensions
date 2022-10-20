6905bbc604c7418e10108041c7203af6f2f52f56
Kevin DeJong <kddejong@amazon.com>
2022-10-20 07:00:41 -0700

    Return errors when given from guard (#61)

--- packages/cfn_guard_rs/python/tests/test_cfn_guard_rs.py
+++ packages/cfn_guard_rs/python/tests/test_cfn_guard_rs.py
@@ -3,6 +3,7 @@
 """
 import yaml
 import pytest
+import cfn_guard_rs.errors
 from cfn_guard_rs import Comparison, run_checks, DataOutput, NameInfo
 
 
@@ -66,7 +67,7 @@ from cfn_guard_rs import Comparison, run_checks, DataOutput, NameInfo
                     "S3_BUCKET_LEVEL_PUBLIC_ACCESS_PROHIBITED": [
                         NameInfo(
                             rule="S3_BUCKET_LEVEL_PUBLIC_ACCESS_PROHIBITED",
-                            #pylint: disable=line-too-long
+                            # pylint: disable=line-too-long
                             path="/Resources/Bucket/Properties/PublicAccessBlockConfiguration/BlockPublicAcls",
                             provided="false",
                             expected="true",
@@ -86,10 +87,36 @@ from cfn_guard_rs import Comparison, run_checks, DataOutput, NameInfo
 )
 def test_run_checks(template, rules, expected):
     """Test transactions against run_checks"""
-    with open(template, encoding='utf8') as file:
+    with open(template, encoding="utf8") as file:
         template_str = yaml.safe_load(file)
-    with open(rules, encoding='utf8') as file:
+    with open(rules, encoding="utf8") as file:
         rules = file.read()
 
     result = run_checks(template_str, rules)
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "template,rules,error",
+    [
+        (
+            "python/tests/fixtures/templates/s3_bucket_public_access_valid.yaml",
+            "python/tests/fixtures/rules/invalid_missing_value.guard",
+            cfn_guard_rs.errors.MissingValue,
+        ),
+        (
+            "python/tests/fixtures/templates/s3_bucket_public_access_valid.yaml",
+            "python/tests/fixtures/rules/invalid_format.guard",
+            cfn_guard_rs.errors.ParseError,
+        ),
+    ],
+)
+def test_run_checks_errors(template, rules, error):
+    """Test transactions against run_checks"""
+    with open(template, encoding="utf8") as file:
+        template_str = yaml.safe_load(file)
+    with open(rules, encoding="utf8") as file:
+        rules = file.read()
+
+    with pytest.raises(error):
+        run_checks(template_str, rules)
--- packages/cfn_guard_rs/src/lib.rs
+++ packages/cfn_guard_rs/src/lib.rs
@@ -1,20 +1,34 @@
 use pyo3::prelude::*;
-use cfn_guard;
+use cfn_guard::{run_checks, ValidateInput};
+mod errors;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn run_checks_rs(data: &str, rules: &str, verbose: bool) -> PyResult<String> {
-    let result = match cfn_guard::run_checks(data, rules, verbose)
-    {
-        Ok(t) => t,
-        Err(e) => (e.to_string()),
-    };
+fn run_checks_rs(data: &str, rules: &str, verbose: bool) -> Result<String, errors::CfnGuardError> {
+    let result = run_checks(ValidateInput { content: data, file_name: ""}, ValidateInput { content: rules, file_name: ""}, verbose)?;
     Ok(result)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn cfn_guard_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("JsonError", _py.get_type::<errors::JsonError>())?;
+    m.add("YamlError", _py.get_type::<errors::YamlError>())?;
+    m.add("FormatError", _py.get_type::<errors::FormatError>())?;
+    m.add("IoError", _py.get_type::<errors::IoError>())?;
+    m.add("RegexError", _py.get_type::<errors::RegexError>())?;
+    m.add("ParseError", _py.get_type::<errors::ParseError>())?;
+    m.add("MissingProperty", _py.get_type::<errors::MissingProperty>())?;
+    m.add("MissingVariable", _py.get_type::<errors::MissingVariable>())?;
+    m.add("MultipleValues", _py.get_type::<errors::MultipleValues>())?;
+    m.add("IncompatibleRetrievalError", _py.get_type::<errors::IncompatibleRetrievalError>())?;
+    m.add("IncompatibleError", _py.get_type::<errors::IncompatibleError>())?;
+    m.add("NotComparable", _py.get_type::<errors::NotComparable>())?;
+    m.add("ConversionError", _py.get_type::<errors::ConversionError>())?;
+    m.add("Errors", _py.get_type::<errors::Errors>())?;
+    m.add("RetrievalError", _py.get_type::<errors::RetrievalError>())?;
+    m.add("MissingValue", _py.get_type::<errors::MissingValue>())?;
+    m.add("FileNotFoundError", _py.get_type::<errors::FileNotFoundError>())?;
     m.add_function(wrap_pyfunction!(run_checks_rs, m)?)?;
     Ok(())
 }
