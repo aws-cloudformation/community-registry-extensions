7d9a66ae46cb5d147b0564564445a65c21043058
Eric Z. Beard <ericzbeard@gmail.com>
2022-12-14 14:28:07 -0800

    chore(release): Move common build policy and webhooks to a common template (#107)
    * Move common build policy and webhooks to a common template
    
    * Moving awscommunity cicd to a folder
    
    Co-authored-by: Eric Beard <ezbeard@amazon.com>

--- release/deploy-cicd.sh
+++ release/deploy-cicd.sh
@@ -7,14 +7,9 @@
 
 set -eou pipefail
 
-STACK_NAME="cep-${CEP_ENV}"
-TEMPLATE_DIR="."
-
-if [ ! -z "${1:-}" ]
-then
-    TEMPLATE_DIR=$1
-    STACK_NAME="cep-${CEP_ENV}-${TEMPLATE_DIR}"
-fi
+COMMON_STACK_NAME="cep-common-${CEP_ENV}"
+TEMPLATE_DIR=$1
+STACK_NAME="cep-${CEP_ENV}-${TEMPLATE_DIR}"
 TEMPLATE_FILE="${TEMPLATE_DIR}/cicd.yml"
 
 if [ -z "${GIT_BRANCH:-}" ]
@@ -37,9 +32,16 @@ then
     PREFIX="Not-needed-for-3p-templates"
 fi
 
+# Deploy the common template, then the namespace-specific template
+
+cfn-lint common.yml -i W3002,W2001
 cfn-lint $TEMPLATE_FILE -i W3002,W2001
 
-aws --profile $PROFILE cloudformation package --template-file $TEMPLATE_FILE --s3-bucket $PACKAGE_BUCKET --output-template-file ${CEP_ENV}-package.yml
+# Deploy the common stack
+aws --profile $PROFILE cloudformation package --template-file common.yml --s3-bucket $PACKAGE_BUCKET > ${CEP_ENV}-common-package.yml
+rain --profile $PROFILE deploy --params Env=$CEP_ENV,GitUrl=$GIT_URL,GitBranch=$GIT_BRANCH,GitHubSecretArn=$GITHUB_SECRET_ARN ${CEP_ENV}-common-package.yml $COMMON_STACK_NAME
 
-rain --profile $PROFILE deploy --params Env=$CEP_ENV,Prefix=$PREFIX,PrefixLower=$PREFIX_LOWER,GitUrl=$GIT_URL,GitBranch=$GIT_BRANCH,GitHubSecretArn=$GITHUB_SECRET_ARN,ProdAccountId=$PROD_ACCOUNT_ID,NotificationEmail=$NOTIFICATION_EMAIL,BetaAccountId=$BETA_ACCOUNT_ID ${CEP_ENV}-package.yml $STACK_NAME
+# Deploy the namespace-specific stack
+aws --profile $PROFILE cloudformation package --template-file $TEMPLATE_FILE --s3-bucket $PACKAGE_BUCKET > ${CEP_ENV}-package.yml
+rain --profile $PROFILE deploy --params Env=$CEP_ENV,PrefixLower=$PREFIX_LOWER,ProdAccountId=$PROD_ACCOUNT_ID,NotificationEmail=$NOTIFICATION_EMAIL,BetaAccountId=$BETA_ACCOUNT_ID ${CEP_ENV}-package.yml $STACK_NAME
 
