f65f5a8f0d1e74cc1629b459759d6b9590099301
Ben Bridts <ben.bridts@gmail.com>
2023-01-07 21:01:56 +0100

    Add AwsCommunity::Account::AlternateContact (#120)

---
+++ resources/Account_AlternateContact/src/awscommunity_account_alternatecontact/models.py
@@ -0,0 +1,87 @@
+# DO NOT modify this file by hand, changes will be overwritten
+from dataclasses import dataclass
+
+from cloudformation_cli_python_lib.interface import (
+    BaseModel,
+    BaseResourceHandlerRequest,
+)
+from cloudformation_cli_python_lib.recast import recast_object
+from cloudformation_cli_python_lib.utils import deserialize_list
+
+import sys
+from inspect import getmembers, isclass
+from typing import (
+    AbstractSet,
+    Any,
+    Generic,
+    Mapping,
+    MutableMapping,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+)
+
+T = TypeVar("T")
+
+
+def set_or_none(value: Optional[Sequence[T]]) -> Optional[AbstractSet[T]]:
+    if value:
+        return set(value)
+    return None
+
+
+@dataclass
+class ResourceHandlerRequest(BaseResourceHandlerRequest):
+    # pylint: disable=invalid-name
+    desiredResourceState: Optional["ResourceModel"]
+    previousResourceState: Optional["ResourceModel"]
+    typeConfiguration: Optional["TypeConfigurationModel"]
+
+
+@dataclass
+class ResourceModel(BaseModel):
+    AccountId: Optional[str]
+    AlternateContactType: Optional[str]
+    EmailAddress: Optional[str]
+    Name: Optional[str]
+    PhoneNumber: Optional[str]
+    Title: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_ResourceModel"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_ResourceModel"]:
+        if not json_data:
+            return None
+        dataclasses = {n: o for n, o in getmembers(sys.modules[__name__]) if isclass(o)}
+        recast_object(cls, json_data, dataclasses)
+        return cls(
+            AccountId=json_data.get("AccountId"),
+            AlternateContactType=json_data.get("AlternateContactType"),
+            EmailAddress=json_data.get("EmailAddress"),
+            Name=json_data.get("Name"),
+            PhoneNumber=json_data.get("PhoneNumber"),
+            Title=json_data.get("Title"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_ResourceModel = ResourceModel
+
+
+@dataclass
+class TypeConfigurationModel(BaseModel):
+    @classmethod
+    def _deserialize(
+        cls: Type["_TypeConfigurationModel"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_TypeConfigurationModel"]:
+        if not json_data:
+            return None
+        return cls()
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_TypeConfigurationModel = TypeConfigurationModel
