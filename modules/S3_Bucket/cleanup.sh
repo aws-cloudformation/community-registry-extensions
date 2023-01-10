f88870a9ec3fb8d65f38a45f0cc8d46568ef95fe
Eric Beard <ezbeard@amazon.com>
2023-01-10 08:24:40 -0800

    Troubleshoot cleanup

--- modules/S3_Bucket/cleanup.sh
+++ modules/S3_Bucket/cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Remove the buckets
 # They are set to retain in the module so they will be left after stack deletion
+aws s3 ls
 aws s3 rb s3://cep-integ-s3-bucket-module
 aws s3 rb s3://cep-integ-log-s3-bucket-module
