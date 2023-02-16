2a5071b43fa995a31a10bba83e66d2ba37a19a33
Kevin DeJong <kddejong@amazon.com>
2023-02-16 15:47:19 -0800

    Release v0.2.1 of cfn-guard-rs-hook (#155)

--- packages/cfn_guard_rs_hook/cfn_guard_rs_hook/types.py
+++ packages/cfn_guard_rs_hook/cfn_guard_rs_hook/types.py
@@ -2,28 +2,29 @@
     Primary Guard type conversion
 """
 from dataclasses import dataclass
-from typing import Callable, Any
+from typing import Any, Callable
 
 
 @dataclass
-class Converter():
+class Converter:
     """
-        Converts types using JSON Patch and a conversion function
+    Converts types using JSON Patch and a conversion function
     """
+
     path: str
     converter: Callable[[str], Any]
 
 
 def to_int(value: str) -> int:
-    """ Convert string to int"""
+    """Convert string to int"""
     return int(value)
 
 
 def to_bool(value: str) -> bool:
-    """ Convert string to bool"""
+    """Convert string to bool"""
     return bool(value)
 
 
 def to_float(value: str) -> float:
-    """ Convert string to float"""
+    """Convert string to float"""
     return float(value)
