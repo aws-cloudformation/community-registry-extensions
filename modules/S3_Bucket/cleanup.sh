a53a343783c4be9d7a9db146192ea2049dad3d6a
Eric Beard <ezbeard@amazon.com>
2023-03-02 09:47:39 -0800

    Change s3 bucket module to Delete

--- modules/S3_Bucket/cleanup.sh
+++ modules/S3_Bucket/cleanup.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # 
-# Remove the integ buckets created by test/integ.yml
-# They are set to retain in the module so they will be left after stack deletion
 
-ACCOUNT_ID=$(aws sts get-caller-identity|jq -r .Account)
-echo "ACCOUNT_ID is $ACCOUNT_ID"
-aws s3 ls
-aws s3 rb s3://cep-integ-s3-bucket-module-${ACCOUNT_ID}
-aws s3 rb s3://cep-integ-log-s3-bucket-module-${ACCOUNT_ID}
 
