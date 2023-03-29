5084e9611199f71da783c192698020d9aec03fba
Eric Beard <ezbeard@amazon.com>
2023-03-29 10:07:38 -0700

    Add params for cloudfront s3 website module

--- modules/CloudFront_S3Website/setup.sh
+++ modules/CloudFront_S3Website/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 #
+# Expected env vars:
+#   ACM_CERTIFICATE_ARN
+#   HOSTED_ZONE_ID
+#
+cat test/integ-template.yml | sed "s/ACM_CERTIFICATE_ARN/${ACM_CERTIFICATE_ARN}/g" | sed "s/HOSTED_ZONE_ID/${HOSTED_ZONE_ID}/g" > test/integ.yml
+
+
 
