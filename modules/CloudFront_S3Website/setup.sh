f8f01f8ab40d89c39edd02e4654001c68d7e2927
Eric Beard <ezbeard@amazon.com>
2023-03-29 11:48:52 -0700

    Fix module setup script

--- modules/CloudFront_S3Website/setup.sh
+++ modules/CloudFront_S3Website/setup.sh
@@ -4,7 +4,9 @@
 #   ACM_CERTIFICATE_ARN
 #   HOSTED_ZONE_ID
 #
-cat test/integ-template.yml | sed "s/ACM_CERTIFICATE_ARN/${ACM_CERTIFICATE_ARN}/g" | sed "s/HOSTED_ZONE_ID/${HOSTED_ZONE_ID}/g" > test/integ.yml
+echo "ACM_CERTIFICATE_ARN is ${ACM_CERTIFICATE_ARN}"
+echo "HOSTED_ZONE_ID is ${HOSTED_ZONE_ID}" 
+cat test/integ-template.yml | sed "s#ACM_CERTIFICATE_ARN#${ACM_CERTIFICATE_ARN}#g" | sed "s/HOSTED_ZONE_ID/${HOSTED_ZONE_ID}/g" > test/integ.yml
 
 
 
