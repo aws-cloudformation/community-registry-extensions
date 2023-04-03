cbc3077ea82249e0ace3e520913290f36d389fe4
Eric Z. Beard <ericzbeard@gmail.com>
2023-04-03 10:55:30 -0700

    Fix setup script for ALIAS (#184)
    * Add CICD for CloudFront S3 Website module
    
    * Crypto <40
    
    * Crypto less than 40 in build image
    
    * Crypto <40
    
    * pip install double quotes for less than
    
    * Add params for cloudfront s3 website module
    
    * Fix module setup script
    
    * Fix CloudFront S3 module integ
    
    * Roles for CloudFront S3 module integ testing
    
    * Fix alias for integ test
    
    * R53 role for CloudFrontS3 module
    
    * Placeholder py test
    
    * Fix DDB templates
    
    * crypto<40
    
    * Readme update
    
    * Make the alias for the CloudFront S3 module configurable
    
    * Setup script to sed ALIAS
    
    ---------
    
    Co-authored-by: Eric Beard <ezbeard@amazon.com>

--- modules/CloudFront_S3Website/setup.sh
+++ modules/CloudFront_S3Website/setup.sh
@@ -3,10 +3,12 @@
 # Expected env vars:
 #   ACM_CERTIFICATE_ARN
 #   HOSTED_ZONE_ID
+#   ALIAS
 #
 echo "ACM_CERTIFICATE_ARN is ${ACM_CERTIFICATE_ARN}"
 echo "HOSTED_ZONE_ID is ${HOSTED_ZONE_ID}" 
-cat test/integ-template.yml | sed "s#ACM_CERTIFICATE_ARN#${ACM_CERTIFICATE_ARN}#g" | sed "s/HOSTED_ZONE_ID/${HOSTED_ZONE_ID}/g" > test/integ.yml
+echo "ALIAS is ${ALIAS}" 
+cat test/integ-template.yml | sed "s#ALIAS#${ALIAS}#g" | sed "s#ACM_CERTIFICATE_ARN#${ACM_CERTIFICATE_ARN}#g" | sed "s/HOSTED_ZONE_ID/${HOSTED_ZONE_ID}/g" > test/integ.yml
 
 
 
