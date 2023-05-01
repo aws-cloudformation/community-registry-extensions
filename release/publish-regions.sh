4ab87dae21468ace3949f5983d844417c42a56e1
Matteo Rinaudo <rinaudom@amazon.com>
2023-05-01 17:33:07 -0400

    Test with one region.

--- release/publish-regions.sh
+++ release/publish-regions.sh
@@ -13,8 +13,8 @@
 EXT_TYPE=$(cat .rpdk-config | jq -r .artifact_type)
 echo "EXT_TYPE is $EXT_TYPE"
 
-regions_to_publish=(us-east-1 af-south-1 ap-east-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ap-southeast-3 ca-central-1 eu-central-1 eu-north-1 eu-south-1 eu-west-1 eu-west-2 eu-west-3 me-central-1 me-south-1 sa-east-1 us-east-2 us-west-1 us-west-2 eu-central-2 eu-south-2 ap-south-2)
-#regions_to_publish=(us-east-1)
+#regions_to_publish=(us-east-1 af-south-1 ap-east-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ap-southeast-3 ca-central-1 eu-central-1 eu-north-1 eu-south-1 eu-west-1 eu-west-2 eu-west-3 me-central-1 me-south-1 sa-east-1 us-east-2 us-west-1 us-west-2 eu-central-2 eu-south-2 ap-south-2)
+regions_to_publish=(us-east-1)
 
 # Errors (known issues with a few regions that we have to skip for now):
 
