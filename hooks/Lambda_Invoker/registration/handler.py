21d50be74c839e9d15f72e12001cedad3c3f22b2
Eric Beard <ericzbeard@gmail.com>
2023-05-23 15:55:18 -0700

    Registration handler

--- hooks/Lambda_Invoker/registration/handler.py
+++ hooks/Lambda_Invoker/registration/handler.py
@@ -1,5 +1,6 @@
 "This custom resource lambda is used from setup.yaml to register a sample Lambda"
 
+import boto3
 from crhelper import CfnResource
 
 helper = CfnResource()
@@ -9,6 +10,22 @@ helper = CfnResource()
 def create_reg(event, _):
     "Create the registration entry in the table"
     print("create_reg called")
+    print(event)
+    props = event["ResourceProperties"]
+    table_arn = props["RegistrationTableArn"]
+    print("table_arn", table_arn)
+    function_arn = props["FunctionArn"]
+    print("function_arn", function_arn)
+    table_name = table_arn.split(":table/")[1]
+    print("table_name", table_name)
+    ddb = boto3.client("dynamodb")
+    ddb.put_item(TableName=table_name, 
+                 Item={
+                     "pk": {"S": function_arn}, 
+                     "sk": {"S": "001"}
+                 })
+    print(f"Put {function_arn} into {table_name}")
+
 
 @helper.delete
 def delete_reg(_, __):
@@ -18,4 +35,6 @@ def delete_reg(_, __):
 def handler(event, context):
     "Handle the invocation from the CloudFormation custom resource"
     helper(event, context)
+
+
 
--- hooks/Lambda_Invoker/src/awscommunity_lambda_invoker/handlers.py
+++ hooks/Lambda_Invoker/src/awscommunity_lambda_invoker/handlers.py
@@ -1,7 +1,9 @@
+"Handler functions for the Lambda Invoker hook"
+#pylint:disable=W0613
 import logging
-from typing import Any, MutableMapping, Optional
+import json
+import traceback
 from cloudformation_cli_python_lib import (
-    BaseHookHandlerRequest,
     HandlerErrorCode,
     Hook,
     HookInvocationPoint,
@@ -11,7 +13,7 @@ from cloudformation_cli_python_lib import (
     exceptions,
 )
 
-from .models import HookHandlerRequest, TypeConfigurationModel
+from .models import TypeConfigurationModel
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
@@ -22,29 +24,24 @@ hook = Hook(TYPE_NAME, TypeConfigurationModel)
 test_entrypoint = hook.test_entrypoint
 
 @hook.handler(HookInvocationPoint.CREATE_PRE_PROVISION)
-def pre_create_handler(
-        session: Optional[SessionProxy],
-        request: HookHandlerRequest,
-        callback_context: MutableMapping[str, Any],
-        type_configuration: TypeConfigurationModel
-) -> ProgressEvent:
+def pre_create_handler(session, request, callback_context, type_configuration):
+    "Called before creating a resource"
     target_model = request.hookContext.targetModel
-    progress: ProgressEvent = ProgressEvent(
-        status=OperationStatus.IN_PROGRESS
-    )
+    progress = ProgressEvent(status=OperationStatus.IN_PROGRESS)
 
     try:
         # Read the Resource Hook's target properties
         resource_properties = target_model.get("resourceProperties")
+        LOG.debug(resource_properties)
 
         if isinstance(session, SessionProxy):
-            ddb = session.client("dynamodb")
-            lam = session.client("lambda")
+            #ddb = session.client("dynamodb")
+            #lam = session.client("lambda")
 
             # TODO
 
             LOG.debug("About to query DDB for all Lambda Arns")
-            LOG.debug(f"DDB Arn: {type_configuration.RegistrationTableArn}")
+            LOG.debug("DDB Arn: %s", type_configuration.RegistrationTableArn)
             # Query the DDB table for all Lambda Arns
 
             # Invoke each Lambda Arn and send in the resource properties
@@ -56,48 +53,43 @@ def pre_create_handler(
             # If anything failed, append all error messages to the progress event message
 
         else:
-            raise Exceptions.InternalFailure("No SessionProxy")
+            raise exceptions.InternalFailure("No SessionProxy")
 
         progress.status = OperationStatus.SUCCESS
-    except TypeError as e:
-        # exceptions module lets CloudFormation know the type of failure that occurred
-        raise exceptions.InternalFailure(f"was not expecting type {e}")
-        # this can also be done by returning a failed progress event
-        # return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
+
+    except Exception as e:
+        return failure(HandlerErrorCode.InternalFailure, str(e), traceback.format_exc())
 
     return progress
 
-
 @hook.handler(HookInvocationPoint.UPDATE_PRE_PROVISION)
-def pre_update_handler(
-        session: Optional[SessionProxy],
-        request: BaseHookHandlerRequest,
-        callback_context: MutableMapping[str, Any],
-        type_configuration: TypeConfigurationModel
-) -> ProgressEvent:
+def pre_update_handler(session, request, callback_context, type_configuration):
+    "Called before updating a resource"
     target_model = request.hookContext.targetModel
-    progress: ProgressEvent = ProgressEvent(
-        status=OperationStatus.IN_PROGRESS
-    )
+    progress = ProgressEvent(status=OperationStatus.IN_PROGRESS)
     try:
         # Read the Resource Hook's target current properties and previous properties
         resource_properties = target_model.get("resourceProperties")
+        LOG.debug(resource_properties)
         previous_properties = target_model.get("previousResourceProperties")
+        LOG.debug(previous_properties)
 
         progress.status = OperationStatus.SUCCESS
-    except TypeError as e:
-        progress = ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
+
+    except Exception as e:
+        return failure(HandlerErrorCode.InternalFailure, str(e), traceback.format_exc())
 
     return progress
 
-
 @hook.handler(HookInvocationPoint.DELETE_PRE_PROVISION)
-def pre_delete_handler(
-        session: Optional[SessionProxy],
-        request: BaseHookHandlerRequest,
-        callback_context: MutableMapping[str, Any],
-        type_configuration: TypeConfigurationModel
-) -> ProgressEvent:
-    return ProgressEvent(
-        status=OperationStatus.SUCCESS
+def pre_delete_handler(session, request, callback_context, type_configuration):
+    "Called before deleting a resource"
+    return ProgressEvent(status=OperationStatus.SUCCESS)
+
+def failure(handler_error_code, error_message, traceback_content):
+    "Log an error, and return a ProgressEvent indicating a failure."
+    LOG.debug("%s\n%s", error_message, traceback_content)
+    return ProgressEvent.failed(
+        handler_error_code,
+        f"Error: {error_message}",
     )
