1624bc1e5f0840b0a3edafd11fe3b01e89ab232e
Eric Beard <ericzbeard@gmail.com>
2023-06-05 15:14:27 -0700

    readme changes

--- hooks/Lambda_Invoker/samples/rain/lambda/index.py
+++ hooks/Lambda_Invoker/samples/rain/lambda/index.py
@@ -6,6 +6,15 @@ def handler(event, context):
     try:
         # This is here to demonstrate the use of a dependency
         r = requests.get("https://www.amazon.com")
+
+        # Add whatever compliance code you need here.
+        
+        # The event has the following properties:
+        #
+        # event["resource_name"]         e.g. AWS::S3::Bucket
+        # event["operation"]             create|update|delete
+        # event["resource_properties"]   {}
+
     except Exception as e:
         print(str(e))
 
